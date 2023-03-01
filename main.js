2f049add83d6bddeca0da8e14f335a5ab51f7504
Pablo Araya <pabloalejaraya@gmail.com>
2023-03-01 04:34:11 -0300

    Se agregó la lista de productos desde javascript

--- main.js
+++ main.js
@@ -17,6 +17,11 @@ const menuIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCart = document.querySelector('.navbar-shopping-cart');
 const productDetail = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+const productList = [];
+
+addProduct();
+renderProducts(productList);
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuIcon.addEventListener('click', toggleMobileMenu);
@@ -36,4 +41,42 @@ function toggleproductDetail() {
     productDetail.classList.toggle('inactive');
     desktopMenu.classList.add("inactive");
     mobileMenu.classList.add("inactive");
+}
+
+function renderProducts (arr) {
+    for (product of productList) {
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+        const img = document.createElement("img");
+        img.setAttribute("src", product.image)
+        productCard.appendChild(img);
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
+        productCard.appendChild(productInfo);
+        const productInfoDiv = document.createElement("div");
+        productInfo.appendChild(productInfoDiv);
+        const productInfoPrice = document.createElement("p");
+        productInfoPrice.innerText = "$" + product.price;
+        productInfoDiv.appendChild(productInfoPrice);
+        const productInfoName = document.createElement("p");
+        productInfoName.innerText = product.name;
+        productInfoDiv.appendChild(productInfoName);
+        const productInfoFigure = document.createElement("figure");
+        productInfo.appendChild(productInfoFigure);
+        const productInfoFigureImg = document.createElement("img");
+        productInfoFigureImg.setAttribute("src", "./icons/bt_add_to_cart.svg")
+        productInfoFigure.appendChild(productInfoFigureImg);
+        
+        cardsContainer.appendChild(productCard)
+    }    
+}
+
+function addProduct() {
+    for (let i = 0; i < 10; i++) {
+        productList.push({
+            name: "Bike" + i,
+            price: 120 + i,
+            image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        });
+    }
 }
