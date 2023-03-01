ebed4470bdfed17742358950bf76d27e67e6be41
Pablo Araya <pabloalejaraya@gmail.com>
2023-03-01 03:15:37 -0300

    Se agregó el carrito de compra

--- main.js
+++ main.js
@@ -10,19 +10,30 @@
 //         desktopMenu.item(0).style.display = "block"
 //     }
 // }
-
+// navbar-shopping-cart  product-detail
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuIcon.addEventListener('click', toggleMobileMenu);
+shoppingCart.addEventListener('click', toggleproductDetail);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
+  productDetail.classList.add("inactive");
 }
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+    productDetail.classList.add("inactive");
   }
+
+function toggleproductDetail() {
+    productDetail.classList.toggle('inactive');
+    desktopMenu.classList.add("inactive");
+    mobileMenu.classList.add("inactive");
+}
